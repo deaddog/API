6895d09f77c160b43eed9d7cfe0c16cb93e9d500
deaddog <mikkelslsl@gmail.com>
2015-07-20 09:43:37 +0200

    Cleaned using-statements.

--- JsonRequestHandler.cs
+++ JsonRequestHandler.cs
@@ -1,11 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
 using System.IO;
+using System.Net;
+using System.Text;
 
 namespace API
 {
